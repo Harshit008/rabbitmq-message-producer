0abb427be15c90f305d3f64dfcaee97a8420af59
Harshit <srivastavaharshit008@gmail.com>
2022-01-07 13:37:22 +0530

    Generated JSON order messages and added eureka client config

--- src/main/java/com/zensar/controller/MessageProducerController.java
+++ src/main/java/com/zensar/controller/MessageProducerController.java
@@ -1,13 +1,29 @@
 package com.zensar.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.zensar.beans.JsonOrderBean;
+import com.zensar.config.MessageConfig;
 
 @RestController
 public class MessageProducerController {
 
-	@GetMapping("/testApi")
-	public String testApi() {
-		return "Welcome to MacysOrder-message-producer app!";
+	@Autowired
+	RabbitTemplate template;
+	
+	@PostMapping("/orderMessage")
+	public String testApi(@RequestBody JsonOrderBean order) throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		//String json = ow.writeValueAsString(order);
+		template.convertAndSend(MessageConfig.EXCHANGE, MessageConfig.ROUTING_KEY, order);
+		return "Success!!";
+		
 	}
 }
