486dd726f24f95d18d9e539b0407d09af45e34e1
Harshit <srivastavaharshit008@gmail.com>
2022-01-17 19:14:58 +0530

    Changes for API gateway config

--- src/main/java/com/zensar/controller/MessageProducerController.java
+++ src/main/java/com/zensar/controller/MessageProducerController.java
@@ -9,13 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zensar.beans.JsonOrderBean;
 import com.zensar.beans.XmlFulfilmentOrderBean;
 import com.zensar.config.MessageConfig;
 
-
+@RequestMapping("/producer")
 @RestController
 public class MessageProducerController {
 
